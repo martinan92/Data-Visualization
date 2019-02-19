5f0b51d24c947c115426a1ec150bf2aa6ad9d835
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-02-19 14:49:08 +0100

    Add files via upload
    Updates needed, strategy is to now switch to different graphs given user input. 
    
    See:
    https://stackoverflow.com/questions/48312392/shiny-allow-users-to-choose-which-plot-outputs-to-display
    https://stackoverflow.com/questions/49387688/change-graph-when-clicking-checkbox-in-shiny-app

--- Leisure_Shiny_App.R
+++ Leisure_Shiny_App.R
@@ -14,15 +14,37 @@ load("leisure_edges.Rda")
 load("leisure_key.Rda")
 
 graph <- graph.data.frame(leisure_edges, vertices = leisure_key, directed=F)
-nodes <- data.frame(id = V(graph)$name, name = V(graph)$Full.name, group = V(graph)$Region, country = V(graph)$Country.of.Birth,
-                    program = V(graph)$Academic.Program)
-edges <- get.data.frame(graph, what="edges")[1:length(graph) +1]
+
+#Overall graph
+nodes <- data.frame(id = V(graph)$name, name = V(graph)$Full.name, group = V(graph)$Region, 
+                          country = V(graph)$Country.of.Birth, program = V(graph)$Academic.Program)
+edges_total <- leisure_edges
+
+#Region graph
+selection<-c('from','to','Region_weight')
+edges_region <- leisure_edges[selection]
+edges_region<- edges_region[edges_region$Region_weight > 0,]
+
+#Country graph
+selection<-c('from','to','Country_weight')
+edges_country <- leisure_edges[selection]
+edges_country<- edges_country[edges_country$Country_weight > 0,]
+
+#Program graph
+selection<-c('from','to','Academic_weight')
+edges_program <- leisure_edges[selection]
+edges_program<- edges_program[edges_program$Program_weight > 0,]
 
 ############################################################################################################ 
 ############################################################################################################ 
 
 #Show charts for the balanced list stocks using default quantmod chart settings in a Shiny App
 ui <- fluidPage(
+  titlePanel("sample data"),
+  sidebarPanel( 
+    selectInput("COUNTRY", "select country", choices=c("India","US", "UK"), selected ="India"), 
+    radioButtons("MONTH","select MONTH", choices=c("jan","feb"))
+  ), 
   visNetworkOutput("network"), 
   dataTableOutput("nodes_data_from_shiny"),
   uiOutput('dt_UI') 
@@ -30,11 +52,40 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$network <- renderVisNetwork({
-    visNetwork(nodes, edges, height = "100%", width = "100%") %>%
+    visNetwork(nodes, edges_total, height = "100%", width = "100%") %>%
     visIgraphLayout(layout = "layout_in_circle") %>%
     visOptions(selectedBy = "group") %>%
     visPhysics(stabilization = F) %>%
     visEvents(select = "function(nodes) {
+                Shiny.onInputChange('current_node_id', nodes.nodes);
+                ;}")
+  })
+  
+  output$network <- renderVisNetwork({
+    visNetwork(nodes, edges_region, height = "100%", width = "100%") %>%
+      visIgraphLayout(layout = "layout_in_circle") %>%
+      visOptions(selectedBy = "group") %>%
+      visPhysics(stabilization = F) %>%
+      visEvents(select = "function(nodes) {
+                Shiny.onInputChange('current_node_id', nodes.nodes);
+                ;}")
+  })
+  output$network <- renderVisNetwork({
+    visNetwork(nodes, edges_country, height = "100%", width = "100%") %>%
+      visIgraphLayout(layout = "layout_in_circle") %>%
+      visOptions(selectedBy = "group") %>%
+      visPhysics(stabilization = F) %>%
+      visEvents(select = "function(nodes) {
+                Shiny.onInputChange('current_node_id', nodes.nodes);
+                ;}")
+  })
+  
+  output$network <- renderVisNetwork({
+    visNetwork(nodes, edges_program, height = "100%", width = "100%") %>%
+      visIgraphLayout(layout = "layout_in_circle") %>%
+      visOptions(selectedBy = "group") %>%
+      visPhysics(stabilization = F) %>%
+      visEvents(select = "function(nodes) {
                 Shiny.onInputChange('current_node_id', nodes.nodes);
                 ;}")
   })

8d08ed9b1d292a3bf5cad6084c27da0066f04729
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-02-19 20:52:58 +0100

    Add files via upload

--- Leisure_Shiny_App.R
+++ Leisure_Shiny_App.R
@@ -10,11 +10,16 @@ library(shiny)
 if(!"igraph" %in% installed.packages()) {install.packages("igraph")}
 library(igraph)
 
+#Load for MBD Cleaning File
 load("leisure_edges.Rda")
 load("leisure_key.Rda")
 
 graph <- graph.data.frame(leisure_edges, vertices = leisure_key, directed=F)
+nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region, 
+                    Country = V(graph)$Country.of.Birth, Program = V(graph)$Academic.Program)
+edges <- leisure_edges
 
+############################################################################################################ 
 ############################################################################################################ 
 ############################################################################################################ 
 
@@ -28,22 +33,23 @@ ui <- fluidPage(
                 , selected ="Overall")),
   visNetworkOutput("network"), 
   dataTableOutput("nodes_data_from_shiny"),
-  uiOutput('dt_UI') 
+  uiOutput('dt_UI1'), 
+  uiOutput('dt_UI2')
 )
 
 server <- function(input, output, session) {
+  #Allows user to choose which connection type to analyze
   changing_data <- reactive({
     req(input$CONNECTION)
-    edges <- leisure_edges
     selection<-c('from','to', paste(input$CONNECTION, "weight", sep="_"))
-    output_edges<-edges[,selection]
-    output_edges<-output_edges[output_edges[length(output_edges)] > 0,]
+    edges<-edges[,selection]
+    edges<-edges[edges[length(edges)] > 0,]
+    edges
   })
   
+  #Allows user to choose which student groupings to analyze
   changing_data2 <- reactive({
-    req(input$FILTER)
-    nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region, 
-                        Country = V(graph)$Country.of.Birth, Program = V(graph)$Academic.Program)
+    req(input$CONNECTION)
     
     if (input$FILTER != 'Overall'){
       nodes$group <- nodes[,c(input$FILTER)]
@@ -55,6 +61,7 @@ server <- function(input, output, session) {
   
   output$network <- renderVisNetwork({
     visNetwork(changing_data2(), changing_data(), height = "100%", width = "100%") %>%
+    visOptions(highlightNearest = TRUE) %>%
     visIgraphLayout(layout = "layout_in_circle") %>%
     visLegend(position = "right", main = "Group") %>%
     visEvents(select = "function(nodes) {
@@ -68,13 +75,43 @@ server <- function(input, output, session) {
     myNode$selected <<- input$current_node_id
   })
   
-  output$table <- renderDataTable({
+  #Creates table of summary info of selected node
+  changing_data3 <- reactive({
+    req(input$FILTER)
+    output_edges <- changing_data()
+    
+    #Check if node has any connections on given filter
+    if (sum(myNode$selected == output_edges$from) > 0){
+      connections <- output_edges[which(myNode$selected == output_edges$from),]
+      top_connections <- connections[connections[length(connections)] == max(connections[length(connections)]),]
+      connection_summary <- merge(top_connections, nodes, by.x = "to", by.y = "id", all.x = T)
+      connection_summary[,3:ncol(connection_summary)]
+    } else{
+    #Otherwise return a blank table
+        setNames(data.frame(matrix(ncol = 5, nrow = 0)), 
+                 c(colnames(output_edges[1]), colnames(nodes[,2:length(nodes)])))
+    }
+  })
+  
+  #Prints the node summary table
+  output$table1 <- renderDataTable({
     nodes[which(myNode$selected == nodes$id),]
   })
   
-  output$dt_UI <- renderUI({
+  #Prints the connection summary table
+  output$table2 <- renderDataTable({
+    changing_data3()
+  })
+  
+  output$dt_UI1 <- renderUI({
     if(nrow(nodes[which(myNode$selected == nodes$id),])!=0){
-      dataTableOutput('table')
+      dataTableOutput('table1')
+    } else{}
+  })
+  
+  output$dt_UI2 <- renderUI({
+    if(nrow(nodes[which(myNode$selected == nodes$id),])!=0){
+      dataTableOutput('table2')
     } else{}
   })
 }

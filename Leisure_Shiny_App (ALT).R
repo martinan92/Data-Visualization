85ab6b489a5a1943363f67e883e7ddc17ae181ff
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-02-20 17:33:39 +0100

    Add files via upload

--- Leisure_Shiny_App (ALT).R
+++ Leisure_Shiny_App (ALT).R
@@ -17,28 +17,25 @@ load("leisure_key.Rda")
 graph <- graph.data.frame(leisure_edges, vertices = leisure_key, directed=F)
 nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region, 
                     Country = V(graph)$Country.of.Birth, Program = V(graph)$Academic.Program)
+
+degree_value <- degree(graph, mode="total")
+nodes$value <- degree_value[match(nodes$id, names(degree_value))]
 nodes$group <- nodes$Region
-nodes$cat <-nodes$id
+
 edges <- leisure_edges
+edges$value <- edges[,length(edges)]
 
 ############################################################################################################ 
 ############################################################################################################ 
 ############################################################################################################ 
-
-filterList <- list(
-  Overall = list(),
-  Region = sort(unique(nodes$Region)),
-  Country = sort(unique(nodes$Country)),
-  Program = sort(unique(nodes$Program))
-)
 
 ui <- fluidPage(
   titlePanel("Leisure Network Analysis"),
   sidebarPanel( 
-    selectInput("FILTER", "Select filter of interest", names(filterList)),
-    # selectInput("CATEGORY", "Select a category", filterList[[1]]),
+    # selectInput("FILTER", "Select filter of interest", choices=c("Overall", "Region", "Country",
+    #               "Program"), selected ="Overall"),
     selectInput("CONNECTION", "Select connections of interest", choices=c("Overall", "Region", "Country", 
-                "Program", "Music_Genre", "Artist", "Sport_Genre", "Athlete", "Movie_Genre", "Movie")
+                  "Program", "Music_Genre", "Artist", "Sport_Genre", "Athlete", "Movie_Genre", "Movie")
                 , selected ="Overall")),
   visNetworkOutput("network"), 
   dataTableOutput("nodes_data_from_shiny"),
@@ -53,27 +50,41 @@ server <- function(input, output, session) {
     selection<-c('from','to', paste(input$CONNECTION, "weight", sep="_"))
     edges<-edges[,selection]
     edges<-edges[edges[length(edges)] > 0,]
+    edges$value<-edges[,length(edges)]
     edges
   })
   
-  #Allows user to choose which student groupings to analyze
-  # observe({
-  #   updateSelectInput(session, "CATEGORY", choices = filterList[[input$FILTER]])
-  # })
-
+ # # Allows user to choose which student groupings to analyze
+ #  changing_data2 <- reactive({
+ #    req(input$CONNECTION)
+ # 
+ #    if (input$FILTER != 'Overall'){
+ #      nodes$group <- nodes[,c(input$FILTER)]
+ #    }else{
+ #      nodes$group <- nodes$Name
+ #    }
+ #    nodes
+ #  })
+  
   changing_data2 <- reactive({
-    req(input$FILTER)
+    req(input$CONNECTION)
     
-    nodes$cat <-filterList[[input$FILTER]]
+    graph <- graph.data.frame(changing_data(), vertices = nodes, directed=F)
+    nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region, 
+                        Country = V(graph)$Country.of.Birth, Program = V(graph)$Academic.Program)
+    
+    nodes$degree <- degree(graph, mode="total")
     nodes
   })
   
   output$network <- renderVisNetwork({
-    visNetwork(changing_data2(), changing_data(), height = "100%", width = "100%") %>%
-    visOptions(selectedBy = "cat") %>%
-    visIgraphLayout(layout = "layout_in_circle") %>%
-    visLegend(position = "right", main = "Group") %>%
-    visEvents(select = "function(nodes) {
+    visNetwork(nodes, changing_data(), height = "100%", width = "100%") %>%
+      visOptions(highlightNearest = TRUE) %>%
+      visNodes(scaling = list(min = 10, max = 50)) %>%
+      visEdges(scaling = list(min = 1, max = 20)) %>%
+      visIgraphLayout(layout = "layout_in_circle") %>%
+      visLegend(position = "right", main = "Group") %>%
+      visEvents(select = "function(nodes) {
                 Shiny.onInputChange('current_node_id', nodes.nodes);
                 ;}")
   })
@@ -86,7 +97,7 @@ server <- function(input, output, session) {
   
   #Creates table of summary info of selected node
   changing_data3 <- reactive({
-    req(input$FILTER)
+    req(input$CONNECTION)
     output_edges <- changing_data()
     
     #Check if node has any connections on given filter
@@ -96,9 +107,9 @@ server <- function(input, output, session) {
       connection_summary <- merge(top_connections, nodes, by.x = "to", by.y = "id", all.x = T)
       connection_summary[,3:ncol(connection_summary)]
     } else{
-    #Otherwise return a blank table
-        setNames(data.frame(matrix(ncol = 5, nrow = 0)), 
-                 c(colnames(output_edges[1]), colnames(nodes[,2:length(nodes)])))
+      #Otherwise return a blank table
+      setNames(data.frame(matrix(ncol = 5, nrow = 0)), 
+               c(colnames(output_edges[1]), colnames(nodes[,2:length(nodes)])))
     }
   })
   

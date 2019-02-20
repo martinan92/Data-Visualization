8752f87d63fe0f52956602b06ec26931c18cb8f0
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-02-20 18:09:41 +0100

    Add files via upload

--- Leisure_Shiny_App (ALT).R
+++ Leisure_Shiny_App (ALT).R
@@ -15,15 +15,10 @@ load("leisure_edges.Rda")
 load("leisure_key.Rda")
 
 graph <- graph.data.frame(leisure_edges, vertices = leisure_key, directed=F)
-nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region, 
+nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region,
                     Country = V(graph)$Country.of.Birth, Program = V(graph)$Academic.Program)
 
-degree_value <- degree(graph, mode="total")
-nodes$value <- degree_value[match(nodes$id, names(degree_value))]
-nodes$group <- nodes$Region
 
-edges <- leisure_edges
-edges$value <- edges[,length(edges)]
 
 ############################################################################################################ 
 ############################################################################################################ 
@@ -32,8 +27,6 @@ edges$value <- edges[,length(edges)]
 ui <- fluidPage(
   titlePanel("Leisure Network Analysis"),
   sidebarPanel( 
-    # selectInput("FILTER", "Select filter of interest", choices=c("Overall", "Region", "Country",
-    #               "Program"), selected ="Overall"),
     selectInput("CONNECTION", "Select connections of interest", choices=c("Overall", "Region", "Country", 
                   "Program", "Music_Genre", "Artist", "Sport_Genre", "Athlete", "Movie_Genre", "Movie")
                 , selected ="Overall")),
@@ -47,6 +40,7 @@ server <- function(input, output, session) {
   #Allows user to choose which connection type to analyze
   changing_data <- reactive({
     req(input$CONNECTION)
+    edges <- leisure_edges
     selection<-c('from','to', paste(input$CONNECTION, "weight", sep="_"))
     edges<-edges[,selection]
     edges<-edges[edges[length(edges)] > 0,]
@@ -54,31 +48,27 @@ server <- function(input, output, session) {
     edges
   })
   
- # # Allows user to choose which student groupings to analyze
- #  changing_data2 <- reactive({
- #    req(input$CONNECTION)
- # 
- #    if (input$FILTER != 'Overall'){
- #      nodes$group <- nodes[,c(input$FILTER)]
- #    }else{
- #      nodes$group <- nodes$Name
- #    }
- #    nodes
- #  })
-  
   changing_data2 <- reactive({
     req(input$CONNECTION)
+    if(input$CONNECTION != "Overall"){
+      graph <- graph.data.frame(changing_data(), vertices = leisure_key, directed=F)
+    } else{
+      graph <- graph.data.frame(leisure_edges, vertices = leisure_key, directed=F)
+    }  
     
-    graph <- graph.data.frame(changing_data(), vertices = nodes, directed=F)
-    nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region, 
+    nodes <- data.frame(id = V(graph)$name, Name = V(graph)$Full.name, Region = V(graph)$Region,
                         Country = V(graph)$Country.of.Birth, Program = V(graph)$Academic.Program)
     
-    nodes$degree <- degree(graph, mode="total")
+    nodes$group <- nodes$Region
+    degree_value <- degree(graph, mode="total")
+    nodes$degree <- degree_value[match(nodes$id, names(degree_value))]
+    nodes$value <- nodes$degree
+    
     nodes
   })
   
   output$network <- renderVisNetwork({
-    visNetwork(nodes, changing_data(), height = "100%", width = "100%") %>%
+    visNetwork(changing_data2(), changing_data(), height = "100%", width = "100%") %>%
       visOptions(highlightNearest = TRUE) %>%
       visNodes(scaling = list(min = 10, max = 50)) %>%
       visEdges(scaling = list(min = 1, max = 20)) %>%
@@ -105,17 +95,18 @@ server <- function(input, output, session) {
       connections <- output_edges[which(myNode$selected == output_edges$from),]
       top_connections <- connections[connections[length(connections)] == max(connections[length(connections)]),]
       connection_summary <- merge(top_connections, nodes, by.x = "to", by.y = "id", all.x = T)
-      connection_summary[,3:ncol(connection_summary)]
+      connection_summary$value.x <- NULL
+      connection_summary[,3:ncol(connection_summary)-1]
     } else{
       #Otherwise return a blank table
-      setNames(data.frame(matrix(ncol = 5, nrow = 0)), 
+      setNames(data.frame(matrix(ncol = 8, nrow = 0)), 
                c(colnames(output_edges[1]), colnames(nodes[,2:length(nodes)])))
     }
   })
   
   #Prints the node summary table
   output$table1 <- renderDataTable({
-    nodes[which(myNode$selected == nodes$id),]
+    nodes[which(myNode$selected == nodes$id), 1:length(nodes)-1]
   })
   
   #Prints the connection summary table
